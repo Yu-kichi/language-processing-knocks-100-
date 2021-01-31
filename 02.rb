338ed129c93087d3127e99e79a602d7c09e159e9
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-01-31 14:56:09 +0900

    remove unnecessary files

--- 02.rb
+++
@@ -1,6 +0,0 @@
-patoka = "パトカー"
-takusi = "タクシー"
-
-ary = patoka.chars
-ary2 = takusi.chars
-puts ary.zip(ary2).flatten.join
--- 07.rb
+++
@@ -1,5 +0,0 @@
-def template(x,y,z)
-  "#{x}時の#{y}は#{z}"
-end
-
-puts template(12,"気温",22.4)
